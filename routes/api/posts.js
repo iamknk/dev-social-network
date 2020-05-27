8b63be61ce523da3fe7fcf269ccaa75f8d022045
Kashif Nazir Khan <pb193105@gmail.com>
2020-05-27 14:03:04 +0500

    get and delete post

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -36,4 +36,59 @@ router.post(
   }
 );
 
+//@route GET api/posts
+//@desc Get all posts
+//@access Private
+router.get('/', auth, async (req, res) => {
+  try {
+    const posts = await Post.find().sort({ date: -1 });
+    res.json(posts);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send('Server Error');
+  }
+});
+
+//@route GET api/posts/:id
+//@desc Get Posts by ID
+//@access Private
+router.get('/:id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post) {
+      return res.status(404).json({ msg: 'Post Not Found' });
+    }
+    res.json(post);
+  } catch (error) {
+    if (error.kind === 'ObjectId') {
+      return res.status(404).json({ msg: 'Post Not Found' });
+    }
+    console.log(error);
+    res.status(500).send('Server Error');
+  }
+});
+
+//@route DELETE api/posts/:id
+//@desc Delete Posts by ID
+//@access Private
+router.delete('/:id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post) {
+      return res.status(404).json({ msg: 'Post Not Found' });
+    }
+    if (post.user.toString() !== req.user.id) {
+      return res.status(401).send('User Not Authorized');
+    }
+    await post.remove();
+    res.json({ msg: 'Post Removed' });
+  } catch (error) {
+    if (error.kind === 'ObjectId') {
+      return res.status(404).json({ msg: 'Post Not Found' });
+    }
+    console.log(error);
+    res.status(500).send('Server Error');
+  }
+});
+
 module.exports = router;
