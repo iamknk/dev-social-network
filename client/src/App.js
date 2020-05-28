58d6d98e8086a5ebb483ffe2c24fae92f8b3b4e4
Kashif Nazir Khan <pb193105@gmail.com>
2020-05-29 02:21:10 +0500

    react router added with inital routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,24 @@
 import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
 
 import './App.css';
 
 const App = () => {
   return (
-    <Fragment>
+    <Router>
       <Navbar />
-      <Landing />
-    </Fragment>
+      <Route exact path="/" component={Landing} />
+      <section className="container">
+        <Switch>
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/login" component={Login} />
+        </Switch>
+      </section>
+    </Router>
   );
 };
 
