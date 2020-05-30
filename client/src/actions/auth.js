b8c1bf934a64d30615f0edbb5b5c1ffbe94433c2
Kashif Nazir Khan <pb193105@gmail.com>
2020-05-30 07:03:43 +0500

    logout complete

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -6,6 +6,7 @@ import {
   AUTH_ERROR,
   LOGIN_SUCCESS,
   LOGIN_FAIL,
+  LOGOUT,
 } from './types';
 import Alert, { setAlert } from './alert';
 import setAuthToken from '../utils/setAuthToken';
@@ -77,4 +78,8 @@ export const login = (email, password) => async (dispatch) => {
       type: LOGIN_FAIL,
     });
   }
+};
+
+export const logout = () => (dispatch) => {
+  dispatch({ type: LOGOUT });
 };
