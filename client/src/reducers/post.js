a528736965c74469cee7779fbdf051c311a0ffcd
Kashif Nazir Khan <pb193105@gmail.com>
2020-06-01 23:58:49 +0500

    comment delete

--- client/src/reducers/post.js
+++ client/src/reducers/post.js
@@ -60,7 +60,7 @@ export default function (state = initialState, action) {
     case ADD_COMMENT:
       return {
         ...state,
-        post: { ...state.post, comment: payload },
+        post: { ...state.post, comments: payload },
         loading: false,
       };
     case REMOVE_COMMENT:
